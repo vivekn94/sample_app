9645cd5860171d17f244c27cbfdc85280fbbb948
vivekn94 <viveknagaraju1994@gmail.com>
2016-07-04 15:54:57 +0530

    finish update , edit and delete

--- rails_project/sample_app/app/controllers/users_controller.rb
+++ rails_project/sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+  before_filter :signed_in_user, only: [:edit, :update,:index]
+before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
+
   def new
   	@user = User.new
   end
@@ -19,4 +25,48 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+  	    @users = User.paginate(page: params[:page])
+  end
+
+
+def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+private
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+ def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
 end
--- rails_project/sample_app/app/helpers/sessions_helper.rb
+++ rails_project/sample_app/app/helpers/sessions_helper.rb
@@ -23,5 +23,9 @@ def sign_out
 def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
+
+  def current_user?(user)
+    user == current_user
+  end
   
 end
